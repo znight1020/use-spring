f952ca692dd0a024402e677c7488f58c98d75ea1
이현수 <znight1020@naver.com>
2024-07-12 09:26:34 +0900

    init: jwt-oauth 프로젝트 생성

---
+++ jwt-oauth-sign/src/main/java/training/sign/JwtOauthSignApplication.java
@@ -0,0 +1,13 @@
+package training.sign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOauthSignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOauthSignApplication.class, args);
+	}
+
+}
---
+++ jwt-oauth-sign/src/test/java/training/sign/JwtOauthSignApplicationTests.java
@@ -0,0 +1,13 @@
+package training.sign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOauthSignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
